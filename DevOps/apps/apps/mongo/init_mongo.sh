f6c9df8fa1f6d642e7566feb9ea1882401cd5c54
Robin Cohen-Selmon <87440088+RobsOnWaves@users.noreply.github.com>
2022-08-02 22:39:16 +0200

    First commit, project structure

---
+++ DevOps/apps/apps/mongo/init_mongo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+STR=$(</OSMOS.js)
+
+mongosh --port 27017 --authenticationDatabase "admin" -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD << END_SCRIPT
+$STR
+
+END_SCRIPT
+echo $STR
