ab5643700c8be97f0f5dc244a027a98a4f06f384
Robin Cohen-Selmon <87440088+RobsOnWaves@users.noreply.github.com>
2022-08-03 22:39:16 +0200

    Decomposing sections by type and creating a dictionary by type

--- Code/libs/ged_file_handler.py
+++ Code/libs/ged_file_handler.py
@@ -9,6 +9,39 @@ class GedFileHandler:
         self.file.name
 
     def to_dict(self):
+        first_line = False
         with open(self.file, 'r') as f:
             for line in f.read().split('\n'):
                 print(line)
+                decomposed_line = line.split(' ')
+
+                #handling utf-8 mess
+                if first_line is False:
+                    first_line = True
+                    if decomposed_line[0] == '\ufeff0':
+                        decomposed_line[0] = '0'
+
+                #Starting a new section ?
+                if decomposed_line[0] == '0':
+                    if decomposed_line[1] == 'HEAD':
+                        current_document = {'node_type': 'head'}
+                    if  decomposed_line[1] == 'TRLR':
+                        #end of file
+                        break
+                    else:
+                        try:
+                            if decomposed_line[2] == 'INDI':
+                                current_document = {'node_type': 'person'}
+                            elif decomposed_line[2] == 'FAM':
+                                current_document = {'node_type': 'family'}
+                            elif decomposed_line[2] == 'SUBM':
+                                current_document = {'node_type': 'subm'}
+
+                        except Exception as e:
+                            print('error in parsing levels 0:' + str(e))
+
+                print(decomposed_line)
+                print(current_document)
+
+
+
