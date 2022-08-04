d87f15b5847dcc026fb1063212263ecc1bb1e684
Robin Cohen-Selmon <87440088+RobsOnWaves@users.noreply.github.com>
2022-08-04 09:15:59 +0200

    Reducing complexity of the GedFileHandler.to_dict() function

--- Code/libs/ged_file_handler.py
+++ Code/libs/ged_file_handler.py
@@ -4,9 +4,29 @@ from pathlib import Path
 class GedFileHandler:
     def __init__(self, file: Path):
         self.file = file
+        self.current_document = {}
 
     def get_filename(self):
         self.file.name
+
+    def __handle_new_document__(self, decomposed_line: [str]):
+        # Starting a new section ?
+        if decomposed_line[0] == '0':
+            if decomposed_line[1] == 'HEAD':
+                self.current_document = {'node_type': 'head'}
+            if decomposed_line[1] == 'TRLR':
+                return
+            else:
+                try:
+                    if decomposed_line[2] == 'INDI':
+                        self.current_document = {'node_type': 'person'}
+                    elif decomposed_line[2] == 'FAM':
+                        self.current_document = {'node_type': 'family'}
+                    elif decomposed_line[2] == 'SUBM':
+                        self.current_document = {'node_type': 'subm'}
+
+                except Exception as e:
+                    print('error in parsing levels 0:' + str(e))
 
     def to_dict(self):
         first_line = False
@@ -21,27 +41,9 @@ class GedFileHandler:
                     if decomposed_line[0] == '\ufeff0':
                         decomposed_line[0] = '0'
 
-                #Starting a new section ?
-                if decomposed_line[0] == '0':
-                    if decomposed_line[1] == 'HEAD':
-                        current_document = {'node_type': 'head'}
-                    if  decomposed_line[1] == 'TRLR':
-                        #end of file
-                        break
-                    else:
-                        try:
-                            if decomposed_line[2] == 'INDI':
-                                current_document = {'node_type': 'person'}
-                            elif decomposed_line[2] == 'FAM':
-                                current_document = {'node_type': 'family'}
-                            elif decomposed_line[2] == 'SUBM':
-                                current_document = {'node_type': 'subm'}
-
-                        except Exception as e:
-                            print('error in parsing levels 0:' + str(e))
-
+                self.__handle_new_document__(decomposed_line=decomposed_line)
                 print(decomposed_line)
-                print(current_document)
+                print(self.current_document)
 
 
 
