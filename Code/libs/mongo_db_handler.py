073dab2ed44a1928e40c9513382f5b26aa62e743
Robin Cohen-Selmon <87440088+RobsOnWaves@users.noreply.github.com>
2022-08-20 10:16:45 +0200

    GED-4 : Initiating the MongoGed class

--- Code/libs/mongo_db_handler.py
+++ Code/libs/mongo_db_handler.py
@@ -1,10 +1,25 @@
+from pymongo import MongoClient
+
+
 class MongoDbGed:
 
     def __init__(self, address: str, user: str, password: str):
-        self.address = address
-        self.user = user
-        self.password = password
+        self.__address__ = address
+        self.__user__ = user
+        self.__password__ = password
+        self.__connection_string__ = "mongodb://" + user + ":" + password + "@" + address
+        self.__mongo_client__ = MongoClient(self.__connection_string__)
 
-    def whoami(self):
-        print(self.user)
+    def add_list_of_ged_objets(self, ged_objects: [dict], collection_name: str):
+        db = self.__mongo_client__.GED
+        collection_handler = getattr(db, collection_name)
+        cursor = collection_handler.insert_many(ged_objects)
 
+        if cursor.acknowledged is True:
+            return {"ged_insert_status": "Insertions with success"}
+
+        elif cursor.acknowledged is not True:
+            return {"ged_insert_status": "Insertions_not_ended_mongoDB did not acknowledged"}
+
+        elif cursor.matched_count == 0:
+            return {"ged_insert_status": "Insertionsn_not_ended_mongoDB no document matched"}
--- Code/main.py
+++ Code/main.py
@@ -1,15 +1,13 @@
 from libs.mongo_db_handler import MongoDbGed
 from libs.ged_file_handler import GedFileHandler
 from pathlib import Path
+
 if __name__ == '__main__':
     print('Welcome to the GED handler')
 
-    mongo_handler = MongoDbGed(address='localhost:27017', user='root', password='rootpwdmongo')
-
-    mongo_handler.whoami()
+    mongo_handler = MongoDbGed(address='localhost:27017', user='root', password='rootmongopwd')
 
     ged_handler = GedFileHandler(Path('./data_in/ged_in.ged'))
 
-    print(str(ged_handler.get_filename()))
-
+    mongo_handler.add_list_of_ged_objets(ged_handler.listed_documents, collection_name='benichman')
 
