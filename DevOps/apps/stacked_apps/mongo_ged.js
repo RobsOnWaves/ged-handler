3b04e462a0b97034bf57b59bf50e7045dc7de046
Robin Cohen-Selmon <87440088+RobsOnWaves@users.noreply.github.com>
2022-12-04 22:25:50 +0100

    GED-39: putting the standalone docker-compose.yaml in a dedicated directory

---
+++ DevOps/apps/stacked_apps/mongo_ged.js
@@ -0,0 +1,42 @@
+
+db = new Mongo().getDB("GED");
+
+db = new Mongo().getDB("USERS");
+
+db.users.createIndex({ "user_name": 1 }, { unique: true })
+
+db.users.insert({
+    "user_name": "robinator",
+
+    "robinator": {
+        "username": "robinator",
+        "full_name": "Robin Cohen-Selmon",
+        "email": "robinator@ged-api.com",
+        "hashed_password": "$2b$12$7jcW/wJzwt4yl9naxz8c3ObWqTUMp7kzPxrD2DJC1EmkbzymRsL12",
+        "disabled": false,
+        "created_at": ISODate("1986-06-16T17:00:00.000+02:00"),
+        "created_by": "The One",
+        "role": "admin"
+    }
+
+}
+)
+
+
+db.users.insert({
+    "user_name": "user",
+
+    "user": {
+        "username": "user",
+        "full_name": "user",
+        "email": "user@ged-api.com",
+        "hashed_password": "$2y$10$YxdfAHfGJy.yOl.ZUKUDVe3AH3BSwsrpDe/DgkN89HRT5UYsmQGE6",
+        "disabled": false,
+        "created_at": ISODate("1986-06-16T17:00:00.000+02:00"),
+        "created_by": "The other One",
+        "role": "user"
+    }
+
+}
+)
+
