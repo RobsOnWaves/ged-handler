81639d6b6912f7e44ce7599c914ce170f790f45f
Robin Cohen-Selmon <87440088+RobsOnWaves@users.noreply.github.com>
2023-04-29 10:14:57 +0200

    Enhancing layout of tree

--- Code/gedmongoed_reactjs_client/src/treeview.js
+++ Code/gedmongoed_reactjs_client/src/treeview.js
@@ -1,59 +1,59 @@
 import React from 'react';
 import Tree from 'react-d3-tree';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
 
 // This is a simplified example of an org chart with a depth of 2.
 // Note how deeper levels are defined recursively via the `children` property.
 const OrgChart = {
 
-    "name": {
-        "given_names": ["kookoo"],
-        "family_name": "coconut"
-    },
-    "children": [
+    name: 'CEO',
+  children: [
+    {
+      name: 'Manager',
+      attributes: {
+        department: 'Production',
+      },
+      children: [
         {
-            "name": {
-                "given_names": ["Tristan", "ducoin"],
-                "family_name": "Le Breton"
+          name: 'Foreman',
+          attributes: {
+            department: 'Fabrication',
+          },
+          children: [
+            {
+              name: 'Worker',
             },
-            "children": [],
-            "attributes": {
-                "sex": "male",
-                "birth": {
-                    "date_info": {
-                        "date": "1950-03-07",
-                        "date_type": "full_date"
-                    },
-                    "place": "TUNIS,Tunisie"
-                },
-                "death": {
-                    "date_info": {
-                        "date": "2019-02-12",
-                        "date_type": "full_date"
-                    },
-                    "place": "Toulouse,France"
-                }
-            }
-        }
-    ],
-    "attributes": {
-        "sex": "male",
-        "birth": {
-            "date_info": {
-                "date": "1950-06-06",
-                "date_type": "full_date"
+          ],
+        },
+        {
+          name: 'Foreman',
+          attributes: {
+            department: 'Assembly',
+          },
+          children: [
+            {
+              name: 'Worker',
             },
-            "place": "FORCALQUIER,France"
-        }
-    }
-
-
+          ],
+        },
+      ],
+    },
+  ],
 };
 
 export default function OrgChartTree() {
     return (
-        // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
-        <div id="treeWrapper" style={{ width: '50em', height: '20em' }}>
-            <Tree data={OrgChart} />
-        </div>
+        <Container>
+            <Row className="justify-content-center mt-5">
+                <Col md={6}>
+                    {// `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
+                    }
+                    <div id="treeWrapper" style={{ width: '100em', height: '50em' }}>
+                        <Tree data={OrgChart} />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
