beb18f20cf9ffbbfae3dc15d6f035a4be7c048e7
Robin Cohen-Selmon <87440088+RobsOnWaves@users.noreply.github.com>
2023-12-23 04:24:32 +0100

    ajout des calculs finaux et mise en forme

--- Code/libs/gold_digger.py
+++ Code/libs/gold_digger.py
@@ -8,12 +8,42 @@ from unidecode import unidecode
 import re
 from io import BytesIO
 import numpy as np
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Alignment
 
 class GoldDigger:
     def __init__(self):
         self.__messages__ = Messages()
 
-    async def compute_excel_file(self, upload_file: UploadFile, price_per_g: int):
+    async def compute_excel_file(self, upload_file: UploadFile, price_per_kg: int, gold_coeffs: dict):
+        def extract_weight_and_separate_by_fineness(row):
+            # Initial setup for different fineness categories
+            weights = {'>750 mil': None, '750 mil': None, '585 mil': None, '375 mil': None}
+
+            # Regular expressions for finding weight and fineness
+            weight_pattern = re.compile(r'(\d+(?:\.\d+)?)\s*g')
+            fineness_pattern = re.compile(r'(\d{3,4})\s*mil', re.IGNORECASE)
+            superior_pattern = re.compile(r'superieur a\s*(\d+)\s*mil', re.IGNORECASE)
+
+            # Extracting weight
+            weight_match = weight_pattern.search(row['Designation'])
+            if weight_match:
+                weight = float(weight_match.group(1))
+
+                # Determining fineness category and assigning weight
+                fineness_match = fineness_pattern.search(row['Designation'])
+                superior_match = superior_pattern.search(row['Designation'])
+                if fineness_match and not superior_match:
+                    fineness = fineness_match.group(1) + ' mil'
+                    weights[fineness] = weight
+                elif superior_match:
+                    fineness = ">" + superior_match.group(1) + ' mil'
+                    weights[fineness] = weight
+
+            return pd.Series(weights)
+
+
         def extract_weights2(description):
             # Dictionnaire pour stocker les poids
             weights = {'>750 mil': '', '750 mil': '', '585 mil': '', '375 mil': ''}
@@ -35,27 +65,6 @@ class GoldDigger:
                     weights['375 mil'] = weight
 
             return weights
-
-        def extract_weights(text):
-            # Find all occurrences of patterns like "375 mil = X g"
-            mil_weight_matches = re.findall(r'(\d+)\s*mil\s*=\s*(\d+\.?\d*)\s*g', text)
-
-            processed_matches = {f'{mil} mil': float(weight.replace(',', '.')) for mil, weight in mil_weight_matches}
-
-            # Check if there are no specific weight matches
-            if not processed_matches:
-                # Find the overall weight and assume it corresponds to the single mentioned titrage
-                overall_weight_match = re.search(r'(\d+\.?\d*)\s*g', text)
-                if overall_weight_match:
-                    weight = overall_weight_match.group(1)
-                    # Find the titrage
-                    mil_match = re.search(r'(\d+)\s*mil', text)
-                    if mil_match:
-                        mil = mil_match.group(1)
-                        return {f'{mil} mil': float(weight)}
-
-            # Convert matches to a dictionary with titrage as keys and weights as values
-            return {f'{mil} mil': float(weight) for mil, weight in mil_weight_matches}
 
         def clean_text(text):
             """ Transliterate non-UTF-8 characters to their closest UTF-8 equivalents """
@@ -84,21 +93,14 @@ class GoldDigger:
 
             return df
 
-        price_per_kg = 59597.52
         price_per_g = price_per_kg / 1000
 
         # Extract the table
         df = await docx_table_to_df(upload_file)
 
         df['Designation'] = df['Designation'].str.replace(',', '.')
-        # Assuming 'df' is your DataFrame
-        # # Apply the extraction function to each row in the "Désignation" column
-        # weights_data = df['Designation'].apply(extract_weights)
-        #
-        # # Create new columns for each titrage and fill with weights
-        # for titrage in set().union(*weights_data):
-        #     df[titrage] = weights_data.apply(lambda x: x.get(titrage))
 
+        df['Platine'] = df['Designation'].apply(lambda x: 'x' if 'platine' in x.lower() else "")
         # Créer des colonnes pour chaque titrage dans le DataFrame
         for mil in ['>750 mil', '750 mil', '585 mil', '375 mil']:
             df[mil] = None
@@ -109,31 +111,6 @@ class GoldDigger:
             for key in weight_info:
                 df.at[index, key] = weight_info[key]
 
-        def extract_weight_and_separate_by_fineness(row):
-            # Initial setup for different fineness categories
-            weights = {'>750 mil': None, '750 mil': None, '585 mil': None, '375 mil': None}
-
-            # Regular expressions for finding weight and fineness
-            weight_pattern = re.compile(r'(\d+(?:\.\d+)?)\s*g')
-            fineness_pattern = re.compile(r'(\d{3,4})\s*mil', re.IGNORECASE)
-            superior_pattern = re.compile(r'superieur a\s*(\d+)\s*mil', re.IGNORECASE)
-
-            # Extracting weight
-            weight_match = weight_pattern.search(row['Designation'])
-            if weight_match:
-                weight = float(weight_match.group(1))
-
-                # Determining fineness category and assigning weight
-                fineness_match = fineness_pattern.search(row['Designation'])
-                superior_match = superior_pattern.search(row['Designation'])
-                if fineness_match and not superior_match:
-                    fineness = fineness_match.group(1) + ' mil'
-                    weights[fineness] = weight
-                elif superior_match:
-                    fineness = ">" + superior_match.group(1) + ' mil'
-                    weights[fineness] = weight
-
-            return pd.Series(weights)
 
         mask = df[['>750 mil', '750 mil', '585 mil', '375 mil']].isna() | (
                     df[['>750 mil', '750 mil', '585 mil', '375 mil']] == '')
@@ -163,9 +140,73 @@ class GoldDigger:
         df['750 mil'].fillna(0.0, inplace=True)
         df['>750 mil'].fillna(0.0, inplace=True)
 
-        df['prix (€)'] = ((df['585 mil'] * 585/1000 + df['375 mil'] * 375/1000 + df['750 mil'] * 750/1000) * price_per_g).round(0)
+        df['prix (€) haut'] = ((price_per_g - gold_coeffs['offset_euros']/1000) * \
+                              ( df['585 mil'] * gold_coeffs['coeff_585_nume']/gold_coeffs['coeff_585_nume']
+                                + df['375 mil'] * gold_coeffs['coeff_375_nume']/gold_coeffs['coeff_375_nume']
+                                + df['750 mil'] * gold_coeffs['coeff_750_nume']/gold_coeffs['coeff_750_nume']
+                                + df['>750 mil'] * gold_coeffs['coeff_22up_nume']/gold_coeffs['coeff_22up_denum'])).round(0)
 
+        df['prix (€) bas'] = ((price_per_g - gold_coeffs['offset_euros']/1000) * \
+                              ( df['585 mil'] * gold_coeffs['coeff_585_nume']/gold_coeffs['coeff_585_nume']
+                                + df['375 mil'] * gold_coeffs['coeff_375_nume']/gold_coeffs['coeff_375_nume']
+                                + df['750 mil'] * gold_coeffs['coeff_750_nume']/gold_coeffs['coeff_750_nume']
+                                + df['>750 mil'] * gold_coeffs['coeff_22down_nume']/gold_coeffs['coeff_22down_denum'])).round(0)
+
+        df['prix (€) bas'] = df['prix (€) bas'].astype(object)
+        df['prix (€) haut'] = df['prix (€) haut'].astype(object)
+        df.loc[df['Platine'] == 'x', 'prix (€) haut'] = 'Platine'
+        df.loc[df['Platine'] == 'x', 'prix (€) bas'] = 'Platine'
+        df.loc[df['Platine'] == 0, 'Platine'] = ''
         file_name = './data_out/' + datetime.datetime.now().strftime(
             "%Y%m%d%H%M%S") + '_mon_fichier_excel.xlsx'
-        df.to_excel(file_name, index=False)
+        #df.to_excel(file_name, index=False, sheet_name='Sheet1'
+
+        # Assurez-vous que 'file_name' contient le chemin vers votre fichier Excel
+        file_name = 'path_to_your_file.xlsx'
+
+        # Écrivez votre DataFrame dans le fichier Excel
+        df.to_excel(file_name, index=False, sheet_name='Sheet1')
+
+        # Chargez le fichier Excel pour la mise en forme avec openpyxl
+        book = load_workbook(file_name)
+        sheet = book['Sheet1']
+
+        # Ajustement de la largeur des colonnes autres que 'Designation'
+        for col in sheet.columns:
+            if col[0].column_letter != 'B':  # 'B' est la lettre de la colonne pour 'Designation'
+                max_length = max(len(str(cell.value)) for cell in col if cell.value) + 2
+                sheet.column_dimensions[col[0].column_letter].width = max_length
+
+        # Définir une largeur fixe pour la colonne 'Designation' et activer le texte à la ligne
+        sheet.column_dimensions['B'].width = 70  # Ajustez la largeur selon vos besoins
+        for cell in sheet['B']:
+            cell.alignment = Alignment(wrap_text=True)
+
+        # Ajuster la hauteur des lignes pour accommoder le texte mis à la ligne automatiquement
+        for row in sheet.iter_rows():
+            sheet.row_dimensions[row[0].row].height = 70  # Ajustez la hauteur selon vos besoins
+
+        # Créez des objets d'alignement pour chaque type de cellule
+        alignment_title = Alignment(horizontal='center', vertical='top', wrap_text=True)
+        alignment_designation = Alignment(horizontal='left', vertical='top', wrap_text=True)
+        alignment_center = Alignment(horizontal='center', vertical='center', wrap_text=True)
+
+        # Appliquez l'alignement au titre
+        for cell in sheet[1]:  # La première ligne contient les titres
+            cell.alignment = alignment_title
+
+        # Appliquez l'alignement à la colonne 'Designation'
+        for cell in sheet['B']:
+            cell.alignment = alignment_designation
+
+        # Appliquez l'alignement centré à toutes les autres cellules
+        for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
+            for cell in row:
+                if cell.column_letter != 'B':  # Ne pas réappliquer pour la colonne 'Designation'
+                    cell.alignment = alignment_center
+
+        # Sauvegardez le fichier après la mise en forme
+        book.save(file_name)
+        book.close()
+
         return file_name
